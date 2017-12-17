48079cbf1eb8e4bddc61546e55e1fec2e8913fc0
Sylvain Schmitz <schmitz@lsv.ens-cachan.fr>
2017-12-17 22:43:47 +0100

    find size distribution

---
+++ tools/tex/distribution.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MAX=`grep 'MAX_AST_SIZE =' ../../src/main/java/fr/lsv/xpparser/XPathEntry.java| sed -e 's/[^0-9]*\([0-9]*\).*/\1/'`
+
+for ((i = 1; i < $MAX; ++i))
+do
+    C=`grep "ast size=\"$i\"" ../../benchmark/*.xml | wc -l`
+    echo "$i $C"
+done
+
