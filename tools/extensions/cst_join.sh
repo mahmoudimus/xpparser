79d44a9ae19c0bd62fd8348e5a54a7561dea9c2e
Anthony LICK <anthony.lick@gmail.com>
2017-12-18 01:55:36 +0100

    Fix freevar.sh, and fix the .xml files used

--- tools/extensions/cst_join.sh
+++ tools/extensions/cst_join.sh
@@ -3,9 +3,11 @@ sum=0
 for FILE in ../../benchmark/*xml
 do
   filename=$(basename "$FILE")
-  value=$(xmlstarlet sel -t -c "count(/benchmark/xpath[ast/descendant::*[(local-name()='eqOp' or local-name()='equalOp' or local-name()='neOp' or local-name()='notEqualOp') and */*[local-name()='intergerConstantExpr' or local-name()='decimalConstantExpr' or local-name()='doubleConstantExpr' or local-name()='stringConstantExpr']]])" $FILE)
-  sum=$(($sum+$value))
-  echo "  <source @name='$filename'>$value</source>"
+  if [[ $filename = *"-full.xml" || $filename = "w3c.xml" || $filename = "xpathmark.xml" ]]; then
+    value=$(xmlstarlet sel -t -c "count(/benchmark/xpath[ast/descendant::*[(local-name()='eqOp' or local-name()='equalOp' or local-name()='neOp' or local-name()='notEqualOp') and */*[local-name()='intergerConstantExpr' or local-name()='decimalConstantExpr' or local-name()='doubleConstantExpr' or local-name()='stringConstantExpr']]])" $FILE)
+    sum=$(($sum+$value))
+    echo "  <source @name='$filename'>$value</source>"
+  fi
 done
 echo "  <total>$sum</total>"
 echo "</constantJoin>"
--- tools/extensions/var.sh
+++ tools/extensions/var.sh
@@ -3,9 +3,11 @@ sum=0
 for FILE in ../../benchmark/*xml
 do
   filename=$(basename "$FILE")
-  value=$(xmlstarlet sel -t -c "count(/benchmark/xpath[ast/descendant::*[local-name()='varRef']])" $FILE)
-  sum=$(($sum+$value))
-  echo "  <source @name='$filename'>$value</source>"
+  if [[ $filename = *"-full.xml" || $filename = "w3c.xml" || $filename = "xpathmark.xml" ]]; then
+    value=$(xmlstarlet sel -t -c "count(/benchmark/xpath[ast/descendant::*[local-name()='varRef']])" $FILE)
+    sum=$(($sum+$value))
+    echo "  <source @name='$filename'>$value</source>"
+  fi
 done
 echo "  <total>$sum</total>"
 echo "</var>"
