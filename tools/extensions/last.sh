533b4e36aa51f9e17bd0711172e6b19b0c495203
Sylvain Schmitz <schmitz@lsv.ens-cachan.fr>
2017-12-18 13:49:33 +0100

    added first()

--- tools/extensions/last.sh
+++ tools/extensions/last.sh
@@ -5,7 +5,7 @@ for FILE in ../../benchmark/*-full.xml
 do
   filename=$(basename "$FILE")
   if [[ $filename = *"-full.xml" || $filename = "w3c.xml" || $filename = "xpathmark.xml" ]]; then
-    value=$(xmlstarlet sel -t -c "count(/benchmark/xpath[ast/descendant::*[local-name()='functionName' and .='last']])" $FILE)
+    value=$(xmlstarlet sel -t -c "count(/benchmark/xpath[ast/descendant::*[(local-name()='functionName' and .='last') or (local-name()='predicates' and (child::*[local-name()='integerConstantExpr' and child::* = '1'] or child::*[(local-name()='equalOp' or local-name()='notEqualOp') and child::*/child::*[local-name()='functionCallExpr' and child::* = 'position'] and child::*/child::*[local-name()='integerConstantExpr' and child::* = '1']]))]])" $FILE)
     sum=$(($sum+$value))
     echo "  <source @name='$filename'>$value</source>"
   fi
