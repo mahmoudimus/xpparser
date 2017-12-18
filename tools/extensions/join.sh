7b316a953e60f923ef4d0543662f969d0ab32f0e
Sylvain Schmitz <schmitz@lsv.ens-cachan.fr>
2017-12-18 10:53:05 +0100

    fixed issues with joins

--- tools/extensions/join.sh
+++ tools/extensions/join.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-echo "<fullJoin>"
+echo "<pureJoin>"
 sum=0
 for FILE in ../../benchmark/*-full.xml
 do
   filename=$(basename "$FILE")
   if [[ $filename = *"-full.xml" || $filename = "w3c.xml" || $filename = "xpathmark.xml" ]]; then
     value=$(xmlstarlet sel -t -c "count(/benchmark/xpath[ast/descendant::*[(local-name()='eqOp' or local-name()='equalOp' or local-name()='neOp' or local-name()='notEqualOp') and child::*[local-name()='firstOperand']/child::*[local-name()='pathExpr' or local-name()='contextItemExpr'] and child::*[local-name()='secondOperand']/child::*[local-name()='pathExpr' or local-name()='contextItemExpr']]])" $FILE)
+    sum=$(($sum+$value))
+    echo "  <source @name='$filename'>$value</source>"
+  fi
+done
+echo "  <total>$sum</total>"
+echo "</pureJoin>"
+echo "<fullJoin>"
+sum=0
+for FILE in ../../benchmark/*-full.xml
+do
+  filename=$(basename "$FILE")
+  if [[ $filename = *"-full.xml" || $filename = "w3c.xml" || $filename = "xpathmark.xml" ]]; then
+    value=$(xmlstarlet sel -t -c "count(/benchmark/xpath[ast/descendant::*[(local-name()='eqOp' or local-name()='equalOp' or local-name()='neOp' or local-name()='notEqualOp') and not(child::*[local-name()='firstOperand' or local-name()='secondOperand']/child::*[local-name()='integerConstantExpr' or local-name()='decimalConstantExpr' or local-name()='doubleConstantExpr' or local-name()='stringConstantExpr'])]])" $FILE)
     sum=$(($sum+$value))
     echo "  <source @name='$filename'>$value</source>"
   fi
