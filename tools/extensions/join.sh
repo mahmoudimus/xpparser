fb1b72d7e5132617ba69983f35f6098d1bc6db60
Sylvain Schmitz <schmitz@lsv.ens-cachan.fr>
2017-12-18 04:24:26 +0100

    forgot . = pi in joins

--- tools/extensions/join.sh
+++ tools/extensions/join.sh
@@ -5,7 +5,7 @@ for FILE in ../../benchmark/*-full.xml
 do
   filename=$(basename "$FILE")
   if [[ $filename = *"-full.xml" || $filename = "w3c.xml" || $filename = "xpathmark.xml" ]]; then
-    value=$(xmlstarlet sel -t -c "count(/benchmark/xpath[ast/descendant::*[(local-name()='eqOp' or local-name()='equalOp' or local-name()='neOp' or local-name()='notEqualOp') and child::*[local-name()='firstOperand']/child::*[local-name()='pathExpr'] and child::*[local-name()='secondOperand']/child::*[local-name()='pathExpr']]])" $FILE)
+    value=$(xmlstarlet sel -t -c "count(/benchmark/xpath[ast/descendant::*[(local-name()='eqOp' or local-name()='equalOp' or local-name()='neOp' or local-name()='notEqualOp') and child::*[local-name()='firstOperand']/child::*[local-name()='pathExpr' or local-name()='contextItemExpr'] and child::*[local-name()='secondOperand']/child::*[local-name()='pathExpr' or local-name()='contextItemExpr']]])" $FILE)
     sum=$(($sum+$value))
     echo "  <source @name='$filename'>$value</source>"
   fi
