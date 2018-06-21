26c5653f1f2293169d59c573f635dc8872ad78fe
David Baelde <david.baelde@gmail.com>
2018-06-21 15:27:38 +0200

    More stats in countfuns

--- tools/extensions/countfuns.pl
+++ tools/extensions/countfuns.pl
@@ -29,12 +29,20 @@ print "Generating countfuns_dist.dat...\n";
 open DAT,">","countfuns_dist.dat" or die "Cannot open countfuns_dist.dat!\n";
 $n=0;
 $sofar=0;
+$threshold=100;
 for (sort { $table{$b} <=> $table{$a} } keys %table) {
+  if ($table{$_}<$threshold) {
+    print "* There are $n functions with >=$threshold occurrences,\n";
+    printf("  together they account for %.2f%% of occurrences.\n",
+      100*($sofar/$total));
+    $threshold=0;
+  }
   $n++;
   $sofar+=$table{$_};
   if ($sofar >= 0.7*$total) {
     $sofar=0;
-    print "$n functions (out of $nbfuns) needed to cover 70% of occurrences.\n";
+    print
+      "* $n functions (out of $nbfuns) needed to cover 70% of occurrences.\n";
   }
   print DAT "$n $table{$_}\n";
 }
