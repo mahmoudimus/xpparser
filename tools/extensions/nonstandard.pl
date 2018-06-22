73107761d537d13acc0eb2ba26c6b035638884d5
David Baelde <david.baelde@gmail.com>
2018-06-22 17:31:14 +0200

    Tweaks

--- tools/extensions/nonstandard.pl
+++ tools/extensions/nonstandard.pl
@@ -236,7 +236,7 @@ foreach (@functions) {
 }
 
 $nonstandard = "ast/descendant::*[local-name()='functionName' $query]";
-$inextras = 'schemas/validation[@schema=\'xpath-1.0-core-extra.rnc\' or @schema=\'xpath-efo-extra.rnc\'][@valid=\'yes\']';
+$inextras = 'schemas/validation[@schema=\'xpath-1.0-core-extra.rnc\' or @schema=\'xpath-efo-extra.rnc\' or @schema=\'xpath-1.0-vertical-extra.rnc\' or @schema=\'xpath-2.0-core-extra.rnc\' or @schema=\'xpath-emso2-extra.rnc\' or @schema=\'xpath-non-mixing-extra.rnc\' or @schema=\'xpath-1.0.forward-extra.rnc\'][@valid=\'yes\']';
 
 $total=0;
 for my $file (@files) {
