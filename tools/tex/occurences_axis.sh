4f6a36d0cc98a712da03d6e8fc746ac40bc11379
Anthony Lick <anthony.lick@gmail.com>
2018-09-30 17:21:39 +0200

    Improve occurences tables
    add percentage in both table + 'any axis' line in first table

--- tools/tex/occurences_axis.sh
+++ tools/tex/occurences_axis.sh
@@ -3,6 +3,25 @@
 xslt=`grep 'xslt' benchmarks-all-full.xml | sed -e 's/.*href="\([^"]*\).*/\1/'`
 xquery=`grep 'xquery' benchmarks-all-full.xml | sed -e 's/.*href="\([^"]*\).*/\1/'`
 
+# XSLT queries (total)
+total_xslt=0
+for file in $xslt
+do
+  count=`xmlstarlet sel -N xqx="http://www.w3.org/2005/XQueryX" -t -c "count(/benchmark/xpath)" $file`
+  total_xslt=$(($total_xslt+$count))
+done
+
+# Xquery queries (total)
+total_xquery=0
+for file in $xquery
+do
+  count=`xmlstarlet sel -N xqx="http://www.w3.org/2005/XQueryX" -t -c "count(/benchmark/xpath)" $file`
+  total_xquery=$(($total_xquery+$count))
+done
+
+# Total queries
+total=$(($total_xslt+$total_xquery))
+
 printf '\\begin{tabular}{lrrr}\n'
 printf '\\toprule\n'
 printf 'Axis & XSLT & XQuery & Total \\\\\n'
@@ -20,23 +39,52 @@ do
   count_xslt=0
   for file in $xslt
   do
-    count=`xmlstarlet sel -N xqx="http://www.w3.org/2005/XQueryX" -t -c "count(//xpath[ast//xqx:xpathAxis[. = \"$axis\" or . = \"$axis-or-self\"]])" $file`
+    count=`xmlstarlet sel -N xqx="http://www.w3.org/2005/XQueryX" -t -c "count(/benchmark/xpath[ast//xqx:xpathAxis[. = \"$axis\" or . = \"$axis-or-self\"]])" $file`
     count_xslt=$(($count_xslt+$count))
   done
-  printf $count_xslt
+  percent=`echo "scale=1; 100*$count_xslt/$total_xslt" | bc`
+  printf $count_xslt' ('%.1f'\\%%)' $percent
   printf ' & '
   # XQuery files
   count_xquery=0
   for file in $xquery
   do
-    count=`xmlstarlet sel -N xqx="http://www.w3.org/2005/XQueryX" -t -c "count(//xpath[ast//xqx:xpathAxis[. = \"$axis\" or . = \"$axis-or-self\"]])" $file`
+    count=`xmlstarlet sel -N xqx="http://www.w3.org/2005/XQueryX" -t -c "count(/benchmark/xpath[ast//xqx:xpathAxis[. = \"$axis\" or . = \"$axis-or-self\"]])" $file`
     count_xquery=$(($count_xquery+$count))
   done
-  printf $count_xquery
+  percent=`echo "scale=1; 100*$count_xquery/$total_xquery" | bc`
+  printf $count_xquery' ('%.1f'\\%%)' $percent
   printf ' & '
   count_total=$(($count_xslt+$count_xquery))
-  printf $count_total
+  percent=`echo "scale=1; 100*$count_total/$total" | bc`
+  printf $count_total' ('%.1f'\\%%)' $percent
   printf ' \\\\\n'
 done
+printf '\\midrule\n'
+printf 'Any axis & '
+# XSLT files
+count_xslt=0
+for file in $xslt
+do
+  count=`xmlstarlet sel -N xqx="http://www.w3.org/2005/XQueryX" -t -c "count(/benchmark/xpath[ast//xqx:xpathAxis])" $file`
+  count_xslt=$(($count_xslt+$count))
+done
+percent=`echo "scale=1; 100*$count_xslt/$total_xslt" | bc`
+printf $count_xslt' ('%.1f'\\%%)' $percent
+printf ' & '
+# XQuery files
+count_xquery=0
+for file in $xquery
+do
+  count=`xmlstarlet sel -N xqx="http://www.w3.org/2005/XQueryX" -t -c "count(/benchmark/xpath[ast//xqx:xpathAxis])" $file`
+  count_xquery=$(($count_xquery+$count))
+done
+percent=`echo "scale=1; 100*$count_xquery/$total_xquery" | bc`
+printf $count_xquery' ('%.1f'\\%%)' $percent
+printf ' & '
+count_total=$(($count_xslt+$count_xquery))
+percent=`echo "scale=1; 100*$count_total/$total" | bc`
+printf $count_total' ('%.1f'\\%%)' $percent
+printf ' \\\\\n'
 printf '\\bottomrule\n'
 printf '\\end{tabular}\n'
--- tools/tex/occurences_operators.sh
+++ tools/tex/occurences_operators.sh
@@ -3,6 +3,25 @@
 xslt=`grep 'xslt' benchmarks-all-full.xml | sed -e 's/.*href="\([^"]*\).*/\1/'`
 xquery=`grep 'xquery' benchmarks-all-full.xml | sed -e 's/.*href="\([^"]*\).*/\1/'`
 
+# XSLT queries (total)
+total_xslt=0
+for file in $xslt
+do
+  count=`xmlstarlet sel -N xqx="http://www.w3.org/2005/XQueryX" -t -c "count(/benchmark/xpath)" $file`
+  total_xslt=$(($total_xslt+$count))
+done
+
+# Xquery queries (total)
+total_xquery=0
+for file in $xquery
+do
+  count=`xmlstarlet sel -N xqx="http://www.w3.org/2005/XQueryX" -t -c "count(/benchmark/xpath)" $file`
+  total_xquery=$(($total_xquery+$count))
+done
+
+# Total queries
+total=$(($total_xslt+$total_xquery))
+
 printf '\\begin{tabular}{lrrr}\n'
 printf '\\toprule\n'
 printf 'Operator & XSLT & XQuery & Total \\\\\n'
@@ -18,22 +37,25 @@ do
   count_xslt=0
   for file in $xslt
   do
-    count=`xmlstarlet sel -N xqx="http://www.w3.org/2005/XQueryX" -t -c "count(//xpath[ast//xqx:$name])" $file`
+    count=`xmlstarlet sel -N xqx="http://www.w3.org/2005/XQueryX" -t -c "count(/benchmark/xpath[ast//xqx:$name])" $file`
     count_xslt=$(($count_xslt+$count))
   done
-  printf $count_xslt
+  percent=`echo "scale=1; 100*$count_xslt/$total_xslt" | bc`
+  printf $count_xslt' ('%.1f'\\%%)' $percent
   printf ' & '
   # XQuery files
   count_xquery=0
   for file in $xquery
   do
-    count=`xmlstarlet sel -N xqx="http://www.w3.org/2005/XQueryX" -t -c "count(//xpath[ast//xqx:$name])" $file`
+    count=`xmlstarlet sel -N xqx="http://www.w3.org/2005/XQueryX" -t -c "count(/benchmark/xpath[ast//xqx:$name])" $file`
     count_xquery=$(($count_xquery+$count))
   done
-  printf $count_xquery
+  percent=`echo "scale=1; 100*$count_xquery/$total_xquery" | bc`
+  printf $count_xquery' ('%.1f'\\%%)' $percent
   printf ' & '
   count_total=$(($count_xslt+$count_xquery))
-  printf $count_total
+  percent=`echo "scale=1; 100*$count_total/$total" | bc`
+  printf $count_total' ('%.1f'\\%%)' $percent
   printf ' \\\\\n'
 done
 printf '\\bottomrule\n'
