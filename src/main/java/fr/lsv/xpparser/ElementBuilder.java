f92df00cac1f0c30bfc0da814cbb15e155e884b4
David Baelde <david.baelde@ens-lyon.org>
2019-03-25 17:13:28 +0100

    Updating copyrights in Java code

--- src/main/java/fr/lsv/xpparser/ElementBuilder.java
+++ src/main/java/fr/lsv/xpparser/ElementBuilder.java
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2017 Sylvain Schmitz (ENS Cachan).
+Copyright (C) 2016-2019 Sylvain Schmitz (ENS Paris-Saclay).
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
